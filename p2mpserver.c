3bfa21d2d7e89127f8cf06364ae5f5f8ea59d4c4
Adith Sudhakar <adith.sudhakar@gmail.com>
2011-10-23 19:56:57 -0400

    Server Functionality Completed.

--- p2mpserver.c
+++ p2mpserver.c
@@ -22,8 +22,10 @@ int main(int argc, char *argv[])
 	float drop_pkt;
 	int sock_status_recv;
 	char buf[BUFFER_SIZE];
+	struct node buf_data[atoi(argv[3])];
+        int fill_here,i;
 	
-	int ret = 0, seq_num = 0, type = 0, run_flag = 1,flags=0,prev_seq_num=-1;
+	int ret = 0, seq_num = 0, type = 0, run_flag = 1,flags=0,prev_seq_num=-1,next_there=0;
 
 	char from[INET_ADDRSTRLEN];
 	FILE *fp;
@@ -64,7 +66,7 @@ fp = fopen(serv.filename, "w");
     die("SERVER : file cannot be opened! : ", errno);
   }
 
-
+len=sizeof(sender);
 
 while(run_flag)
 {
@@ -91,52 +93,106 @@ printf("WAITING FOR DATA: \n");
       continue;
     }
 
-	printf("Seq number got is %d \n",seq_num);
+	printf("Seq number got is %d from %s \n",seq_num,from);
 
-drop_pkt=(rand()%1000)/1000.0;
-printf("%f is the DROP rate\n",drop_pkt);
-if(seq_num==prev_seq_num+1&& drop_pkt<serv.p)
-{
+        drop_pkt=(rand()%1000)/1000.0;
 
-printf("TRYING TO WRITE TO FILE \n");
-fwrite(buf+HEADER_SIZE,ret-HEADER_SIZE,1,fp);
-fflush(fp);
-printf("%s",buf);
+        printf("%f is the DROP rate\n",drop_pkt);
 
-pack_data(seq_num, MSG_TYPE_ACK, 0, ack_buf, 8);//CREATE THE ACK
+        if(seq_num==prev_seq_num+1&& drop_pkt>serv.p)
+        {
 
-/*
-check the to_buffer[] struct array repeatedly for any packet that is buffered and has seq_num = cur_seq_num+1;
-if (present) remove that by setting filled=0 
-and write that contents to file
-and set seq_num=seq_num of the packet that was just written
-*/
+        printf("TRYING TO WRITE TO FILE \n");
+        fwrite(buf+HEADER_SIZE,ret-HEADER_SIZE,1,fp);
+        fflush(fp);
+        prev_seq_num=seq_num;
+               
+        /*
+        check the to_buffer[] struct array repeatedly for any packet that is buffered and has seq_num = cur_seq_num+1;
+        if (present) remove that by setting filled=0 
+        and write that contents to file
+        and set seq_num=seq_num of the packet that was just written
+        */
 
-sendto(serv.sock_server_recv, ack_buf, 8, 0, (struct sockaddr*)&sender, sizeof(sender));//SEND THE ACK
-prev_seq_num=seq_num;
-}
+        while(1)
+        {
+                for(i=0;i<serv.N;(i++)%serv.N)
+                {
+                        if(buf_data[i].filled==1 && buf_data[i].seqnum==prev_seq_num+1)
+                        {
+                        fwrite(buf_data[i].buf+HEADER_SIZE,sizeof(buf_data[i].buf)-HEADER_SIZE,1,fp);
+                        fflush(fp);
+                        buf_data[i].filled=0;
+                        prev_seq_num=buf_data[i].seqnum;
+                        }
+                                                      
+                        if(buf_data[i].seqnum==prev_seq_num+1)
+                               next_there=1;
+                }
+        
+                if(next_there!=1)
+                break;
+         }
 
-else if(drop_pkt>serv.p)
-{
-printf("DROPPED PACKET:::: \n");
-}
 
-else{
-
-printf("Out of ORDER PACKET : \n ");
-/* Have to bufer the packet if there is space available in the buffer
- let to_buffer[] be the struct array
-find the slot where filled=0
-push the packet into that slot and set filled=1 
-come out of loop
-if there is no slot then it means window buffer is full. DROP PACKET
-ack(last seq_num); 
-*/
+        
 
 
-}
 
-}
+  
+        pack_data(prev_seq_num, MSG_TYPE_ACK, 0, ack_buf, 8);//CREATE THE ACK
+        printf("Sending ACK FOR %d\n",seq_num);
+        sendto(serv.sock_server_recv, ack_buf, 8, 0, (struct sockaddr*)&sender, sizeof(sender));//SEND THE ACK
+//        prev_seq_num=seq_num;
+        }
+
+        else if(drop_pkt<serv.p)
+        {
+        printf("DROPPED PACKET:::: \n");
+        }
+
+        else{
+
+        printf("Out of ORDER PACKET : \n ");
+        /* Have to bufer the packet if there is space available in the buffer
+         let to_buffer[] be the struct array
+        find the slot where filled=0
+        push the packet into that slot and set filled=1 
+        come out of loop
+        if there is no slot then it means window buffer is full. DROP PACKET
+        ack(last seq_num); 
+        */
+                fill_here=-1;
+                for(i=0;i<serv.N;i++)
+                {
+                if(buf_data[i].filled==0)
+                fill_here=i;
+
+                break;
+                }
+
+           if(fill_here==-1)
+            {
+             printf("OOPs Buffer full cannot save packet. Dropping it ");
+            }
+        
+        strcpy(buf_data[fill_here].buf,buf);
+        buf_data[fill_here].filled=1;
+        buf_data[fill_here].seqnum=seq_num;
+        
+        if(fill_here!=-1)
+               {
+                pack_data(prev_seq_num, MSG_TYPE_ACK, 0, ack_buf, 8);//CREATE THE ACK
+                printf("Sending ACK FOR %d\n",prev_seq_num);
+                sendto(serv.sock_server_recv, ack_buf, 8, 0, (struct sockaddr*)&sender, sizeof(sender));//SEND THE prev ACK
+                }        
+        
+        }
+
+
+printf("\n");
+
+        }
 
 	sock_status_recv = shutdown(serv.sock_server_recv,2);
   	if(sock_status_recv != 0) {
